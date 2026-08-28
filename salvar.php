97866568028a140b54536d7a8b72eca89c6c92af
Eric Vinícius <viniciusdevgit@gmail.com>
2026-08-28 11:40:17 -0300

    Update database connection details in salvar.php
    alteração da parte do localhost

--- salvar.php
+++ salvar.php
@@ -1,9 +1,9 @@
 <?php
 
-  $servidor = "localhost";
-$usuario = "root";
-$senha = "";
-$dbname = "agenda";
+    $host ="sql304.infinityfree.com";
+    $usuario ="if0_42268756";
+    $senha ="8HvdIImZiyp";
+    $banco ="if0_42268756_agenda";
 
 $conexao=mysqli_connect(
     $host,
